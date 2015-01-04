e51aac7ccd159dfda8f64732a9d4869257f885af
Moufid Larbi <moufid4@gmail.com>
2015-01-04 12:09:55 -0500

    Configuring I18n Module

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Internationalization
   class Application < Rails::Application
+config.i18n.default_locale = :en
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
